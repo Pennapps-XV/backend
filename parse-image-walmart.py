bd6d40a47f484ebf49a69ff7820163eb4d9db920
kusti8 <kusti8@gmail.com>
2017-01-21 16:17:03 +0000

    test

--- parse-image-walmart.py
+++ parse-image-walmart.py
@@ -15,7 +15,7 @@ def removes(yes):
 
 def post_some_dict(dict):
     headers = {'Content-type': 'application/json'}
-    r = requests.post("http://127.0.0.1:5000/search", data=json.dumps(dict), headers=headers)
+    r = requests.post("http://45.33.95.66:5000/search", data=json.dumps(dict), headers=headers)
     return r.text
 
 def parse_image(image):
@@ -52,12 +52,6 @@ def parse_wallmart(keywords):
     return json.dumps(out)
 
 class StoreHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-        self.wfile.write(fh.read().encode())
-
     def do_POST(self):
         self.send_response(200)
         length = self.headers['content-length']
