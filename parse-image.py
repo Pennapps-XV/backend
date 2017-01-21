94c10a8702444edad19bba206f47118067ff5f67
kusti8 <kusti8@gmail.com>
2017-01-21 11:16:07 +0000

    Removed walmart requirement

--- parse-image.py
+++ parse-image.py
@@ -6,29 +6,37 @@ from collections import Counter
 image = sys.argv[1]
 threshold = 2
 
+def removes(yes):
+    no = ["Walmart.com", ".", ","]
+    for x in no:
+        yes = yes.replace(x, '')
+    return yes
+
 def post_some_dict(dict):
     headers = {'Content-type': 'application/json'}
     r = requests.post("http://127.0.0.1:5000/search", data=json.dumps(dict), headers=headers)
     return r.text
 
-out = json.loads(post_some_dict({"image_url": image}))['links']
-
-out = [x for x in out if 'walmart' in x]
+out = json.loads(post_some_dict({"image_url": image}))['titles']
+#print(out)
+#out = [x for x in out if 'walmart' in x]
 threshold = len(out)-1
-out = [x[27:-9] for x in out]
-print(out)
+#out = [x[27:-9] for x in out]
+#print(out)
 large = []
 for line in out:
-    line = line.split('-')
+    line = line.replace('-', '')
+    line = removes(line)
+    line = line.split(' ')
     for word in line:
         large.append(word)
-print(large)
+#print(large)
 c = Counter(large).most_common()
 
 keywords = []
 
 for x in c:
     if x[1] > threshold:
-        keywords.append(x)
+        keywords.append(x[0])
 
-print(keywords)
+print(' '.join(keywords))
