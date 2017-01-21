2976a588ff8207f48236052b2fbb91c48e012880
kusti8 <kusti8@gmail.com>
2017-01-21 10:36:48 +0000

    Initial working

--- parse-image.py
+++ parse-image.py
@@ -1,12 +1,34 @@
 import json
 import sys
 import requests
+from collections import Counter
 
 image = sys.argv[1]
+threshold = 2
 
 def post_some_dict(dict):
     headers = {'Content-type': 'application/json'}
     r = requests.post("http://127.0.0.1:5000/search", data=json.dumps(dict), headers=headers)
     return r.text
 
-print(json.loads(post_some_dict({"image_url": image}))['links'][0])
+out = json.loads(post_some_dict({"image_url": image}))['links']
+
+out = [x for x in out if 'walmart' in x]
+threshold = len(out)-1
+out = [x[27:-9] for x in out]
+print(out)
+large = []
+for line in out:
+    line = line.split('-')
+    for word in line:
+        large.append(word)
+print(large)
+c = Counter(large).most_common()
+
+keywords = []
+
+for x in c:
+    if x[1] > threshold:
+        keywords.append(x)
+
+print(keywords)
